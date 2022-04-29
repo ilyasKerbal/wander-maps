ea5269d004eaf9cbdc37c227ed24ef25bf51d222
ILYAS KERBAL <kerbalsc@gmail.com>
2022-04-29 13:29:20 +0000

    Adding Markers

--- app/src/main/java/io/github/ilyaskerbal/wander/MapsActivity.kt
+++ app/src/main/java/io/github/ilyaskerbal/wander/MapsActivity.kt
@@ -42,10 +42,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val latitude = 35.77420452645121
+        val longitude = -5.786890552811697
+        val zoomLevel = 14f
+
+        val homeLatLng = LatLng(latitude, longitude)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        map.addMarker(MarkerOptions().position(homeLatLng).title("My Home"))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
